560c85daf847ee7001e57d1cc0d9c1a3732cb600
Jan Dąbroś <jsd@semihalf.com>
2017-01-27 09:35:33 +0100

    cavium: Always keep eMMC functional in non-secure world
    While booting from MMC, SMMU was configured to treat eMMC transactions
    as secure, thus eMMC wasn't functional in non-secure environment. Some
    customers want to have access to MMC from NS-BL33 and because of that,
    SMMU is restored to initial configuration.
    It's necessary to note, that this solution is a hole in security system
    and should be revisited.
    
    Signed-off-by: Jan Dabros <jan.dabros@cavium.com>

--- plat/cavium/common/bl31_thunder_setup.c
+++ plat/cavium/common/bl31_thunder_setup.c
@@ -148,12 +148,44 @@ static void thunder_el3_irq_init(void)
 		printf("err %d on registering secure handler;\n", rc);
 }
 
+/*
+ * While booting from MMC device, it was necessary to configure SMMU as to grant
+ * access for eMMC controller to secure memory, where images were loaded. On
+ * the other hand in order to keep MMC functional in non-secure world, we must
+ * come back to initial settings. It is safe to do it here, because every
+ * firmware image is loaded at this time.
+ */
+static void thunder_configure_mmc_security(void)
+{
+	int boot_medium;
+	uint64_t val;
+	uint64_t ssd_idx = CAVM_PCC_DEV_CON_E_MIO_EMM >> 5;
+	uint64_t emm_ssd_mask = (1ULL << (CAVM_PCC_DEV_CON_E_MIO_EMM & 0x1F));
+	cavm_gpio_strap_t gpio_strap;
+
+	gpio_strap.u = CSR_READ_PA(0, CAVM_GPIO_STRAP);
+	boot_medium = (gpio_strap.u) & 0x7;
+
+	/* If it isn't MMC boot, then nothing to do here */
+	if (boot_medium != 0x02 && boot_medium != 0x03)
+		return;
+
+	/*
+	 * Configure SMMU and mark MMC controller in NODE0
+	 * as acting for non-secure domain.
+	 */
+	val = CSR_READ_PA(0, CAVM_SMMUX_SSDRX(0, ssd_idx));
+	val |= emm_ssd_mask;
+	CSR_WRITE_PA(0, CAVM_SMMUX_SSDRX(0, ssd_idx), val);
+}
+
 /*******************************************************************************
  * Initialize the gic, configure the CLCD and zero out variables needed by the
  * secondaries to boot up correctly.
  ******************************************************************************/
 void bl31_platform_setup()
 {
+	thunder_configure_mmc_security();
 	thunder_fill_board_details(1);
 	thunder_el3_irq_init();
 	thunder_gic_driver_init();
