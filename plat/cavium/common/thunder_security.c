8b5e77ab1dc88d397382cd64bba365c5418c2dd5
Konrad Adamczyk <konrad.adamczyk@cavium.com>
2017-09-29 11:17:24 +0200

    t93: security: Fix marking physical memory regions as secure/non-secure
    Before this commit, LMC_MODE was always 0, which means that only one
    LMC can have access to particular memory region. This could have
    significant leverage on performance.
    
    Set CCS_LMC_MODE to STRIPE_3 which means that access particular memory
    region will be granted to all three LMCs.
    
    Signed-off-by: Konrad Adamczyk <konrad.adamczyk@cavium.com>

--- plat/cavium/common/thunder_security.c
+++ plat/cavium/common/thunder_security.c
@@ -19,6 +19,15 @@
 
 #define MAX_WAYS 16
 
+/*
+ * Defines used for CN93xx to enable particular
+ * LMC access to particular ASC_REGION
+ */
+#define CAVM_CCS_LMC_MASK_LMC0	0x1
+#define CAVM_CCS_LMC_MASK_LMC1	0x2
+#define CAVM_CCS_LMC_MASK_LMC2	0x4
+
+
 /* Flush the L2 Cache */
 void l2c_flush(void)
 {
@@ -146,27 +155,27 @@ void cn9xxx_security_setup(void)
 		if (region->end > dram_end)
 			region->end = dram_end;
 
-		CSR_WRITE_PA(region->node, CAVM_CCS_ASC_REGIONX_START(region->number), region->start);
-		CSR_WRITE_PA(region->node, CAVM_CCS_ASC_REGIONX_END(region->number), region->end);
+		CSR_WRITE_PA(region->node,
+			     CAVM_CCS_ASC_REGIONX_START(region->number),
+			     region->start);
+		CSR_WRITE_PA(region->node,
+			     CAVM_CCS_ASC_REGIONX_END(region->number),
+			     region->end);
 
 		ccs_asc_attr.u = CSR_READ_PA(region->node, CAVM_CCS_ASC_REGIONX_ATTR(region->number));
-		switch (ccs_asc_attr.s.lmc_mode) {
-			case 0:
-				/* Set first LMC to access this region */
-				ccs_asc_attr.s.lmc_mask = 0x1;
-				break;
-			case 2:
-				ccs_asc_attr.s.lmc_mask = 0x3;
-				break;
-			case 3:
-				ccs_asc_attr.s.lmc_mask = 0x7;
-				break;
-		}
-
+		/*
+		 * For given memory region, grant access
+		 * to this region to all LMCs
+		 */
+		ccs_asc_attr.s.lmc_mode = CAVM_CCS_LMC_MODE_E_STRIPE_3;
+		ccs_asc_attr.s.lmc_mask = CAVM_CCS_LMC_MASK_LMC0 |
+					  CAVM_CCS_LMC_MASK_LMC1 |
+					  CAVM_CCS_LMC_MASK_LMC2;
 		ccs_asc_attr.s.s_en  = region->secure;
 		ccs_asc_attr.s.ns_en = !region->secure;
-
-		CSR_WRITE_PA(region->node, CAVM_CCS_ASC_REGIONX_ATTR(region->number), ccs_asc_attr.u);
+		CSR_WRITE_PA(region->node,
+			     CAVM_CCS_ASC_REGIONX_ATTR(region->number),
+			     ccs_asc_attr.u);
 
 		INFO("Mark memory region %d at node %d:: %lx to %lx as %ssecure (%lx)\n",
 			region->number, region->node,
