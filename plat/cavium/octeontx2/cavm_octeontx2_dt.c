c2c228399656c77bc845e91aa7a62aa87785362c
Nithya <nithya.mani@caviumnetworks.com>
2018-02-28 11:58:06 -0800

    t96: dt: move enum/struct declarations to header
    This patch moves some common enum/struct declarations
    to header file to be used by ECAM driver
    
    Signed-off-by: Nithya <nithya.mani@caviumnetworks.com>

--- plat/cavium/octeontx2/cavm_octeontx2_dt.c
+++ plat/cavium/octeontx2/cavm_octeontx2_dt.c
@@ -19,61 +19,6 @@
 #include <cavm_dt.h>
 #include <cavm_common.h>
 
-/* Define different QLM-MODE variants. These definitions based on the BDK code. */
-typedef enum {
-	CAVM_QLM_MODE_DISABLED,		/* QLM is disabled (all chips) */
-	CAVM_QLM_MODE_PCIE_1X1,		/* 1 PCIe, 1 lane. Other lanes unused */
-	CAVM_QLM_MODE_PCIE_1X2,		/* 1 PCIe, 2 lanes */
-	CAVM_QLM_MODE_PCIE_1X4,		/* 1 PCIe, 4 lanes */
-	CAVM_QLM_MODE_PCIE_1X8,		/* 1 PCIe, 8 lanes */
-	CAVM_QLM_MODE_PCIE_1X16,	/* 1 PCIe, 16 lanes (CN93XX) */
-	CAVM_QLM_MODE_SATA_4X1,		/* SATA, each lane independent (cn88xx) */
-	CAVM_QLM_MODE_SATA_2X1,		/* SATA, each lane independent (cn83xx) */
-	CAVM_QLM_MODE_SGMII_4X1,	/* SGMII, each lane independent (cn88xx) */
-	CAVM_QLM_MODE_SGMII_2X1,	/* SGMII, each lane independent (cn83xx) */
-	CAVM_QLM_MODE_SGMII_1X1,	/* SGMII, single lane (cn80xx) */
-	CAVM_QLM_MODE_XAUI_1X4,		/* 1 XAUI or DXAUI, 4 lanes (cn88xx), use gbaud to tell difference */
-	CAVM_QLM_MODE_RXAUI_2X2,	/* 2 RXAUI, 2 lanes each (cn88xx) */
-	CAVM_QLM_MODE_RXAUI_1X2,	/* 1 RXAUI, 2 lanes each (cn83xx) */
-	CAVM_QLM_MODE_OCI,		/* OCI Multichip interconnect (cn88xx) */
-	CAVM_QLM_MODE_XFI_4X1,		/* 4 XFI, 1 lane each (cn88xx) */
-	CAVM_QLM_MODE_XFI_2X1,		/* 2 XFI, 1 lane each (cn83xx) */
-	CAVM_QLM_MODE_XFI_1X1,		/* 1 XFI, single lane (cn80xx) */
-	CAVM_QLM_MODE_XLAUI_1X4,	/* 1 XLAUI, 4 lanes each (cn88xx) */
-	CAVM_QLM_MODE_10G_KR_4X1,	/* 4 10GBASE-KR, 1 lane each (cn88xx) */
-	CAVM_QLM_MODE_10G_KR_2X1,	/* 2 10GBASE-KR, 1 lane each (cn83xx) */
-	CAVM_QLM_MODE_10G_KR_1X1,	/* 1 10GBASE-KR, single lane (cn80xx) */
-	CAVM_QLM_MODE_40G_KR4_1X4,	/* 1 40GBASE-KR4, 4 lanes each (cn88xx) */
-	CAVM_QLM_MODE_QSGMII_4X1,	/* QSGMII is 4 SGMII on one lane (cn81xx, cn83xx) */
-	CAVM_QLM_MODE_25G_4X1,		/* 25G, 1 lane each (CN93XX QLMs) */
-	CAVM_QLM_MODE_25G_2X1,		/* 25G, 1 lane each (CN93XX DLMs) */
-	CAVM_QLM_MODE_50G_2X2,		/* 50G, 2 lanes each (CN93XX QLMs) */
-	CAVM_QLM_MODE_50G_1X2,		/* 50G, 2 lanes each (CN93XX DLMs) */
-	CAVM_QLM_MODE_100G_1X4,		/* 100G, 4 lanes each (CN93XX) */
-	CAVM_QLM_MODE_25G_KR_4X1,	/* 25G-KR, 1 lane each (CN93XX QLMs) */
-	CAVM_QLM_MODE_25G_KR_2X1,	/* 25G-KR, 1 lane each (CN93XX DLMs) */
-	CAVM_QLM_MODE_50G_KR_2X2,	/* 50G-KR, 2 lanes each (CN93XX QLMs) */
-	CAVM_QLM_MODE_50G_KR_1X2,	/* 50G-KR, 2 lanes each (CN93XX DLMs) */
-	CAVM_QLM_MODE_100G_KR4_1X4,	/* 100G-KR4, 4 lanes each (CN93XX) */
-	CAVM_QLM_MODE_USXGMII_4X1,	/* USXGMII, 4 over 1 lane, 10M, 100M, 1G, 2.5G, 5G (CN93XX QLMs) */
-	CAVM_QLM_MODE_USXGMII_2X1,	/* USXGMII, 2 over 1 lane, 10M, 100M, 1G, 2.5G, 5G, 10G (CN93XX QLMs) */
-	CAVM_QLM_MODE_LAST,
-} cavm_qlm_modes_t;
-
-/* This structure is stored in GSERNX_LANEX_SCRATCHX to remember the mode of a
- * single lane. Based on the BDK structure;
- */
-typedef union
-{
-	uint64_t u;
-	struct {
-		uint16_t baud_mhz;		/* Baudrate of the lane in MHz */
-		cavm_qlm_modes_t mode: 8;	/* Mode of the lane, enumerated by cavm_qlm_modes_t */
-		uint32_t flags: 8;		/* Mode flags */
-		uint32_t reserved;		/* Reserved for future use */
-	} s;
-} cavm_qlm_state_lane_t;
-
 struct qlm_mode_strmap_s {
 	int mode;
 	char *bdk_str;
@@ -914,7 +859,7 @@ static int octeontx2_fdt_get_phy_addr(const void *fdt, int phy_offset)
 }
 
 /* Return number of lanes available for different QLMs. */
-static int octeontx2_get_lane_num(int qlm)
+int octeontx2_get_lane_num(int qlm)
 {
 	if ((qlm == 4) || (qlm == 5)) {
 		return 2;
