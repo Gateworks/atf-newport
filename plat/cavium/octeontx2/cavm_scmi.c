24e597d804790d06b4c560fa77e5de16a23175cd
Nithya Mani <nithya.mani@cavium.com>
2018-06-13 11:40:28 -0700

    t9x: scmi: Fallback to Legacy PM for ASIM model
    This is temp until timing issues are fixed with SCMI
    
    Signed-off-by: Nithya Mani <nithya.mani@cavium.com>

--- plat/cavium/octeontx2/cavm_scmi.c
+++ plat/cavium/octeontx2/cavm_scmi.c
@@ -12,6 +12,7 @@
 **/
 
 #include <arch_helpers.h>
+#include <string.h>
 #include <assert.h>
 #include <debug.h>
 #include <cavm_scmi.h>
@@ -353,6 +354,10 @@ void *scmi_init(scmi_channel_t *ch)
 	cavm_shutdown_config_data_t shutdown_data;
 	uint32_t version;
 	int ret;
+
+	/* Do not use SCMI on ASIM */
+	if (!strncmp(bfdt->board_model, "asim-", 5))
+		return NULL;
 
 	assert(ch && ch->info);
 	assert(ch->info->db_reg_addr);
