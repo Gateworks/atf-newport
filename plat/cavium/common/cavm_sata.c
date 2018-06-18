d2bb20e26c14ea44c121455545d9dd39b9a3cfc9
Konrad Adamczyk <konrad.adamczyk@cavium.com>
2018-06-18 14:06:18 +0200

    cavm: sata: Fixup access to CSRs
    Signed-off-by: Konrad Adamczyk <konrad.adamczyk@cavium.com>

--- plat/cavium/common/cavm_sata.c
+++ plat/cavium/common/cavm_sata.c
@@ -67,12 +67,13 @@ static int sata_drive_check_power_management(int node, int sata)
 {
 	union cavm_satax_uahc_p0_sctl sctl;
 
-	sctl.u = CSR_READ(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_P0_SCTL(sata));
+	sctl.u = CSR_READ(node, CAVM_SATAX_UAHC_P0_SCTL(sata));
+
 	if (sctl.s.ipm != (PM_DISABLE_SLUMBER | PM_DISABLE_PARTIAL)) {
 		sctl.s.ipm = PM_DISABLE_PARTIAL | PM_DISABLE_SLUMBER;
 		debug("ATF: N%d: SATA%d: Force disabling PARTIAL and SLUMBER\n",
 		      node, sata);
-		CSR_WRITE(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_P0_SCTL(sata),
+		CSR_WRITE(node, CAVM_SATAX_UAHC_P0_SCTL(sata),
 			  sctl.u);
 	}
 	return 0;
@@ -103,29 +104,29 @@ static int sata_drive_check_detect_failure(int node, int sata)
 	 * working fine. Also if FIS processing is not started, then
 	 * the SATA controller must not be setup yet.
 	 */
-	cmd.u = CSR_READ(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_P0_CMD(sata));
+	cmd.u = CSR_READ(node, CAVM_SATAX_UAHC_P0_CMD(sata));
 	if (cmd.s.st || !cmd.s.fre || sata_hba[node][sata].sig == SATA_SIG_PMP) {
 		sata_hba[node][sata].state_count = 0;
 		return 0;
 	}
 
 	/* Check if the controller is sending COMRESET (SCTL[DET]=1) */
-	sctl.u = CSR_READ(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_P0_SCTL(sata));
+	sctl.u = CSR_READ(node, CAVM_SATAX_UAHC_P0_SCTL(sata));
 	if (sata_hba[node][sata].state_count && sctl.s.det) {
 		/* We have already started COMRESET, finish it */
 		debug("ATF: N%d: SATA%d: Restart drive detect\n", node, sata);
 		sctl.s.det = 0;
-		CSR_WRITE(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_P0_SCTL(sata),
+		CSR_WRITE(node, CAVM_SATAX_UAHC_P0_SCTL(sata),
 			  sctl.u);
 		sata_hba[node][sata].state_count = 0;
 		return 0;
 	}
 
 	/* See if the controller has detected the drive */
-	ssts.u = CSR_READ(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_P0_SSTS(sata));
+	ssts.u = CSR_READ(node, CAVM_SATAX_UAHC_P0_SSTS(sata));
 	is_detected = (ssts.s.det == SSTS_DET_PHY_DEV_GOOD);
 
-	sig.u = CSR_READ(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_P0_SIG(sata));
+	sig.u = CSR_READ(node, CAVM_SATAX_UAHC_P0_SIG(sata));
 
 	debug("ATF: N%d: SATA%d: Connected device: %x->%x\n", node, sata,
 	      sata_hba[node][sata].sig, sig.s.sig);
@@ -133,7 +134,7 @@ static int sata_drive_check_detect_failure(int node, int sata)
 	sata_hba[node][sata].sig = sig.s.sig;
 
 	/* See if the drive is busy */
-	tfd.u = CSR_READ(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_P0_TFD(sata));
+	tfd.u = CSR_READ(node, CAVM_SATAX_UAHC_P0_TFD(sata));
 	is_busy = ((tfd.s.sts & ATA_TFD_BSY) != 0);
 
 	/*
@@ -151,7 +152,7 @@ static int sata_drive_check_detect_failure(int node, int sata)
 		}
 		debug("ATF: N%d: SATA%d: Failed drive detect\n", node, sata);
 		sctl.s.det = 1;
-		CSR_WRITE(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_P0_SCTL(sata),
+		CSR_WRITE(node, CAVM_SATAX_UAHC_P0_SCTL(sata),
 			  sctl.u);
 		return 0;
 	} else {
@@ -193,8 +194,7 @@ static int sata_drive_check_unplug_failure(int node, int sata)
 	 * lane's EIESTS becomes zero when a drive is hot unplugged.
 	 */
 	for (i = 0; i < NUM_CONTINUOUS_EIE_READ; i++) {
-		detsts.u = CSR_READ(gser_base[node][qlm],
-			            CAVM_GSERX_RX_EIE_DETSTS(qlm));
+		detsts.u = CSR_READ(node, CAVM_GSERX_RX_EIE_DETSTS(qlm));
 		if (detsts.s.eiests & (1 << lane)) {
 			/* EIESTS is set, so a drive is plugged in */
 			goto detected;
@@ -202,7 +202,7 @@ static int sata_drive_check_unplug_failure(int node, int sata)
 	}
 
 	/* See if the controller has detected the drive */
-	ssts.u = CSR_READ(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_P0_SSTS(sata));
+	ssts.u = CSR_READ(node, CAVM_SATAX_UAHC_P0_SSTS(sata));
 	if (ssts.s.det != SSTS_DET_PHY_DEV_GOOD) {
 		/*
 		 * The controller knows there isn't a drive, so we
@@ -211,7 +211,7 @@ static int sata_drive_check_unplug_failure(int node, int sata)
 		goto detected;
 	}
 
-	serr.u = CSR_READ(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_P0_SERR(sata));
+	serr.u = CSR_READ(node, CAVM_SATAX_UAHC_P0_SERR(sata));
 	if (serr.u != 0) {
 		/*
 		 * The controller knows there isn't a drive, so we
@@ -229,8 +229,7 @@ static int sata_drive_check_unplug_failure(int node, int sata)
 	      sata, qlm, lane);
 
 	/* Set the pattern generator to create a static pattern of zeros */
-	CSR_WRITE(gser_base[node][qlm],
-		  CAVM_GSERX_LANEX_LBERT_PAT_CFG(qlm, lane), 0);
+	CSR_WRITE(node, CAVM_GSERX_LANEX_LBERT_PAT_CFG(qlm, lane), 0);
 
 	/* Enable the pattern generator creating 8b/10b patterns of zeros */
 	lbert_cfg.u = CSR_READ(gser_base[node][qlm],
@@ -238,33 +237,28 @@ static int sata_drive_check_unplug_failure(int node, int sata)
 	lbert_cfg.s.lbert_pg_width = 3;
 	lbert_cfg.s.lbert_pg_mode = 8;
 	lbert_cfg.s.lbert_pg_en = 1;
-	CSR_WRITE(gser_base[node][qlm], CAVM_GSERX_LANEX_LBERT_CFG(qlm, lane),
-		  lbert_cfg.u);
+	CSR_WRITE(node, CAVM_GSERX_LANEX_LBERT_CFG(qlm, lane), lbert_cfg.u);
 
 	/* Put the PCS in internal loopback */
-	cfg_0.u = CSR_READ(gser_base[node][qlm],
-			   CAVM_GSERX_LANEX_MISC_CFG_0(qlm, lane));
+	cfg_0.u = CSR_READ(node, CAVM_GSERX_LANEX_MISC_CFG_0(qlm, lane));
 	cfg_0.s.cfg_pcs_loopback = 1;
-	CSR_WRITE(gser_base[node][qlm], CAVM_GSERX_LANEX_MISC_CFG_0(qlm, lane),
-		  cfg_0.u);
-	CSR_READ(gser_base[node][qlm], CAVM_GSERX_LANEX_MISC_CFG_0(qlm, lane));
+	CSR_WRITE(node, CAVM_GSERX_LANEX_MISC_CFG_0(qlm, lane), cfg_0.u);
+	CSR_READ(node, CAVM_GSERX_LANEX_MISC_CFG_0(qlm, lane));
 
 	/* SATA controller should detect link drop */
-	serr.u = CSR_READ(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_P0_SERR(sata));
+	serr.u = CSR_READ(node, CAVM_SATAX_UAHC_P0_SERR(sata));
 
 	/* Disable PCS loopback */
 	cfg_0.s.cfg_pcs_loopback = 0;
-	CSR_WRITE(gser_base[node][qlm], CAVM_GSERX_LANEX_MISC_CFG_0(qlm, lane),
-		  cfg_0.u);
+	CSR_WRITE(node, CAVM_GSERX_LANEX_MISC_CFG_0(qlm, lane), cfg_0.u);
 
 	/* Disable pattern generator */
 	lbert_cfg.s.lbert_pg_en = 0;
-	CSR_WRITE(gser_base[node][qlm], CAVM_GSERX_LANEX_LBERT_CFG(qlm, lane),
-		  lbert_cfg.u);
-	CSR_READ(gser_base[node][qlm], CAVM_GSERX_LANEX_LBERT_CFG(qlm, lane));
+	CSR_WRITE(node, CAVM_GSERX_LANEX_LBERT_CFG(qlm, lane), lbert_cfg.u);
+	CSR_READ(node, CAVM_GSERX_LANEX_LBERT_CFG(qlm, lane));
 
 	/* Clear non-recovered persistent communication error */
-	CSR_WRITE(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_P0_SERR(sata), 0x200);
+	CSR_WRITE(node, CAVM_SATAX_UAHC_P0_SERR(sata), 0x200);
 
 	return 0;
 detected:
@@ -320,8 +314,7 @@ void sata_ipm_quirk()
 		for (sata = 0; sata < sata_ctrlr_count; sata++) {
 			gser = thunder_sata_to_gser(sata);
 
-			gser_cfg.u = CSR_READ(gser_base[node][gser],
-					      CAVM_GSERX_CFG(gser));
+			gser_cfg.u = CSR_READ(node, CAVM_GSERX_CFG(gser));
 			debug("CAVM_GSERX_CFG(%d): %lx\n", gser, gser_cfg.u);
 
 			if (!gser_cfg.s.sata) {
@@ -332,8 +325,7 @@ void sata_ipm_quirk()
 			sata_hba[node][sata].ctrl_base = CSR_PA(node, CAVM_SATAX_PF_BAR0(sata));
 
 			/* check if we are in SATA mode */
-			uctl_ctl.u = CSR_READ(sata_hba[node][sata].ctrl_base,
-					      CAVM_SATAX_UCTL_CTL(sata));
+			uctl_ctl.u = CSR_READ(node, CAVM_SATAX_UCTL_CTL(sata));
 			debug("CAVM_SATAX_UCTL_CTL(%d): %lx\n", sata, uctl_ctl.u);
 
 			if (!uctl_ctl.s.a_clk_en || uctl_ctl.s.a_clkdiv_rst) {
@@ -351,25 +343,25 @@ void sata_ipm_quirk()
 				memset(ahci_cbl, 0, sizeof (struct ahci_command_list) * 32);
 				memset(ahci_cfis, 0, sizeof (struct ahci_command_fis) * 32);
 
-				ghc.u = CSR_READ(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_GBL_GHC(sata));
+				ghc.u = CSR_READ(node, CAVM_SATAX_UAHC_GBL_GHC(sata));
 				ghc.s.hr = 1;
-				CSR_WRITE(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_GBL_GHC(sata), cmd.u);
+				CSR_WRITE(node, CAVM_SATAX_UAHC_GBL_GHC(sata), cmd.u);
 
 				do {
-					ghc.u = CSR_READ(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_GBL_GHC(sata));
+					ghc.u = CSR_READ(node, CAVM_SATAX_UAHC_GBL_GHC(sata));
 					udelay(1);
 				} while (ghc.s.hr);
 
-				cmd.u = CSR_READ(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_P0_CMD(sata));
+				cmd.u = CSR_READ(node, CAVM_SATAX_UAHC_P0_CMD(sata));
 				cmd.s.pod = 1;
 				cmd.s.sud = 1;
-				CSR_WRITE(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_P0_CMD(sata), cmd.u);
+				CSR_WRITE(node, CAVM_SATAX_UAHC_P0_CMD(sata), cmd.u);
 
 				timeout = 100;
 
 				do {
 					mdelay(1);
-					ssts.u = CSR_READ(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_P0_SSTS(sata));
+					ssts.u = CSR_READ(node, CAVM_SATAX_UAHC_P0_SSTS(sata));
 				} while (timeout-- && !ssts.s.det);
 
 				debug("ATF: N%d: SATA%d: SSTS: %x\n", node, sata, ssts.u);
@@ -377,37 +369,37 @@ void sata_ipm_quirk()
 				timeout = 10;
 
 				do {
-					serr.u = CSR_READ(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_P0_SERR(sata));
-					CSR_WRITE(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_P0_SERR(sata), serr.u);
+					serr.u = CSR_READ(node, CAVM_SATAX_UAHC_P0_SERR(sata));
+					CSR_WRITE(node, CAVM_SATAX_UAHC_P0_SERR(sata), serr.u);
 
-					tfd.u = CSR_READ(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_P0_TFD(sata));
+					tfd.u = CSR_READ(node, CAVM_SATAX_UAHC_P0_TFD(sata));
 					tfd.s.sts &= (1 << 7) | (1 << 3) | (1 << 0);
 
 					mdelay(1);
 				} while (timeout-- && tfd.s.sts);
 
-				is.u = CSR_READ(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_P0_IS(sata));
-				CSR_WRITE(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_P0_IS(sata), is.u);
+				is.u = CSR_READ(node, CAVM_SATAX_UAHC_P0_IS(sata));
+				CSR_WRITE(node, CAVM_SATAX_UAHC_P0_IS(sata), is.u);
 
 				clb.u = (uintptr_t)ahci_cbl;
-				CSR_WRITE(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_P0_CLB(sata), clb.u);
+				CSR_WRITE(node, CAVM_SATAX_UAHC_P0_CLB(sata), clb.u);
 
 				fb.u = (uintptr_t)ahci_rfis;
-				CSR_WRITE(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_P0_FB(sata), fb.u);
+				CSR_WRITE(node, CAVM_SATAX_UAHC_P0_FB(sata), fb.u);
 
-				cmd.u = CSR_READ(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_P0_CMD(sata));
+				cmd.u = CSR_READ(node, CAVM_SATAX_UAHC_P0_CMD(sata));
 				cmd.s.fre = 1;
 				cmd.s.icc = 1;
-				CSR_WRITE(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_P0_CMD(sata), cmd.u);
+				CSR_WRITE(node, CAVM_SATAX_UAHC_P0_CMD(sata), cmd.u);
 
 				cmd.s.clo = 1;
-				CSR_WRITE(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_P0_CMD(sata), cmd.u);
+				CSR_WRITE(node, CAVM_SATAX_UAHC_P0_CMD(sata), cmd.u);
 
 				cmd.s.st = 1;
-				CSR_WRITE(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_P0_CMD(sata), cmd.u);
+				CSR_WRITE(node, CAVM_SATAX_UAHC_P0_CMD(sata), cmd.u);
 
-				is.u = CSR_READ(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_P0_IS(sata));
-				CSR_WRITE(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_P0_IS(sata), is.u);
+				is.u = CSR_READ(node, CAVM_SATAX_UAHC_P0_IS(sata));
+				CSR_WRITE(node, CAVM_SATAX_UAHC_P0_IS(sata), is.u);
 
 				ahci_cfis[0].pmnum = 0xF; //PMP
 				ahci_cfis[0].type  = AHCI_FIS_REGISTER_H2D;
@@ -420,21 +412,21 @@ void sata_ipm_quirk()
 				ahci_cbl[0].cmd_cfl = AHCI_FIS_REGISTER_H2D_LENGTH;
 
 				ci.s.ci = 1 << 0;
-				CSR_WRITE(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_P0_CI(sata), ci.u);
+				CSR_WRITE(node, CAVM_SATAX_UAHC_P0_CI(sata), ci.u);
 
 				timeout = 1000;
 
 				do {
 					udelay(1);
-					ci.u = CSR_READ(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_P0_CI(sata));
+					ci.u = CSR_READ(node, CAVM_SATAX_UAHC_P0_CI(sata));
 				} while (timeout-- && ci.s.ci);
 
 				debug("ATF: N%d: SATA%d: TO: %d, CI: %08x\n", node, sata, timeout, ci.s.ci);
 
 				mdelay(1); //Wait 1ms instead of 5us
 
-				is.u = CSR_READ(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_P0_IS(sata));
-				CSR_WRITE(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_P0_IS(sata), is.u);
+				is.u = CSR_READ(node, CAVM_SATAX_UAHC_P0_IS(sata));
+				CSR_WRITE(node, CAVM_SATAX_UAHC_P0_IS(sata), is.u);
 
 				ahci_cbl[0].cmd_c = 0;
 				ahci_cbl[0].cmd_r = 0;
@@ -447,22 +439,22 @@ void sata_ipm_quirk()
 				ahci_cfis[0].control = 0;
 
 				ci.s.ci = 1 << 0;
-				CSR_WRITE(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_P0_CI(sata), ci.u);
+				CSR_WRITE(node, CAVM_SATAX_UAHC_P0_CI(sata), ci.u);
 
 				timeout = 1000;
 
 				do {
 					udelay(1);
-					ci.u = CSR_READ(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_P0_CI(sata));
+					ci.u = CSR_READ(node, CAVM_SATAX_UAHC_P0_CI(sata));
 				} while (timeout-- && ci.s.ci);
 
 				debug("ATF: N%d: SATA%d: TO: %d, CI: %08x\n",
 				      node, sata, timeout, ci.s.ci);
 
-				serr.u = CSR_READ(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_P0_SERR(sata));
-				tfd.u = CSR_READ(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_P0_TFD(sata));
-				sig.u = CSR_READ(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_P0_SIG(sata));
-				is.u = CSR_READ(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_P0_IS(sata));
+				serr.u = CSR_READ(node, CAVM_SATAX_UAHC_P0_SERR(sata));
+				tfd.u = CSR_READ(node, CAVM_SATAX_UAHC_P0_TFD(sata));
+				sig.u = CSR_READ(node, CAVM_SATAX_UAHC_P0_SIG(sata));
+				is.u = CSR_READ(node, CAVM_SATAX_UAHC_P0_IS(sata));
 
 				debug("ATF: N%d: SATA%d: TFD: %02x, SIG: %08x, SERR: %x, IS: %x\n",
 				      node, sata, tfd.s.sts, sig.s.sig, serr.u, is.u);
@@ -472,13 +464,13 @@ void sata_ipm_quirk()
 					      node, sata, i, ahci_rfis[0].d2h_register[i]);
 			} while (retry--);
 
-			cmd.u = CSR_READ(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_P0_CMD(sata));
+			cmd.u = CSR_READ(node, CAVM_SATAX_UAHC_P0_CMD(sata));
 			cmd.s.fre = 0;
 			cmd.s.clo = 0;
 			cmd.s.st = 0;
-			CSR_WRITE(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_P0_CMD(sata), cmd.u);
+			CSR_WRITE(node, CAVM_SATAX_UAHC_P0_CMD(sata), cmd.u);
 
-			sig.u = CSR_READ(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_P0_SIG(sata));
+			sig.u = CSR_READ(node, CAVM_SATAX_UAHC_P0_SIG(sata));
 
 			sata_hba[node][sata].sig = sig.s.sig;
 
@@ -486,9 +478,9 @@ void sata_ipm_quirk()
 			case SATA_SIG_PMP:
 				sata_hba[node][sata].timeout = 0;
 
-				cmd.u = CSR_READ(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_P0_CMD(sata));
+				cmd.u = CSR_READ(node, CAVM_SATAX_UAHC_P0_CMD(sata));
 				cmd.s.pma = 1;
-				CSR_WRITE(sata_hba[node][sata].ctrl_base, CAVM_SATAX_UAHC_P0_CMD(sata), cmd.u);
+				CSR_WRITE(node, CAVM_SATAX_UAHC_P0_CMD(sata), cmd.u);
 
 				break;
 			default:
