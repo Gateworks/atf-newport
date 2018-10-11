9a7d120192b64328d362a2952ad2c3eb3e7d9d90
Konrad Adamczyk <konrad.adamczyk@cavium.com>
2018-10-11 18:11:45 +0200

    marvell: platforms: Implement PPI/SPI APIs
    Each platform can now export own IRQs definitions.
    It results with optimized (linear) usage of SPI IDs.
    
    There is new file cavm_irqs_def.h which collect defines from
    platform_irqs_def.h. If specific device is not defined in platform_def.h
    there is assumption, that this device does not exists.
    
    Change-Id: I1cf4ccd08e061a5252e687a90a8759956e9357d3
    Signed-off-by: Konrad Adamczyk <konrad.adamczyk@cavium.com>

---
+++ include/plat/marvell/octeontx/octeontx_irqs_def.h
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2018 Marvell International Ltd.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ * https://spdx.org/licenses
+ */
+
+#ifndef __OCTEONTX_IRQS_DEF_H__
+#define __OCTEONTX_IRQS_DEF_H__
+
+#include <platform_irqs_def.h>
+
+/*******************************************************************************
+ * If platform doesn't specify defines for device
+ * lets fill it with the default data.
+ * If there is no irq of certain type, than -1 is returned by macro.
+ ******************************************************************************/
+
+/* UAA definitons */
+#ifndef UAA_SPI_IRQ
+/* Default there is no UAA device */
+#define UAA_SPI_IRQ_DEVS		0x0
+#define UAA_SPI_IRQ(dev)		-1
+#endif
+
+/* PEM definitions */
+#ifndef PEM_SPI_IRQ
+/* Default there is no PEM device */
+#define PEM_SPI_IRQ_DEVS		0x0
+#define PEM_SPI_IRQS_PER_DEV	0x0
+#define PEM_SPI_IRQ(dev, irq)	-1
+#endif
+
+/* SMMU definitions */
+#ifndef SMMU_SPI_IRQ
+/* Default there is no SMMU device */
+#define SMMU_SPI_IRQ_DEVS		0x0
+#define SMMU_SPI_IRQS_PER_DEV	0x0
+#define SMMU_SPI_IRQ(dev, irq)	-1
+#endif
+
+/* Base SPI for GPIO interrupt to be handled in ATF */
+#ifndef GPIO_SPI_IRQ
+/* Default there is no GPIO interrupt */
+#define GPIO_SPI_IRQS			0x0
+#define GPIO_SPI_IRQ(irq)		-1
+#endif
+
+/* Default SPI to be used by kernel GPIO driver when intercepting interrupts */
+#ifndef GPIO_SPI_IRQ_NSEC
+/* Default there is no SPI for kernel GPIO driver */
+#define GPIO_SPI_NSEC_IRQS		0x0
+#define GPIO_SPI_IRQ_NSEC(irq)	-1
+#endif
+
+/* BPHY definitions */
+#ifndef BPHY_PSM_IRQ
+#define BPHY_PSM_IRQS_NUMBER	0x0
+#define BPHY_PSM_IRQ(irq)		-1
+#endif
+
+#endif /* __OCTEONTX_IRQS_DEF_H__ */
---
+++ plat/marvell/octeontx/otx/t81/include/platform_irqs_def.h
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2018 Marvell International Ltd.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ * https://spdx.org/licenses
+ */
+
+#ifndef __PLATFORM_IRQS_DEF_H__
+#define __PLATFORM_IRQS_DEF_H__
+
+/*******************************************************************************
+ * Definitions of IRQs
+ ******************************************************************************/
+
+/* Definitions of IRQ SGI (Software Generated Interrupt) IDs (range 0x0 - 0xf) */
+#define SGI_IRQ_OFFSET			0x0
+
+/* Definitions of IRQ PPI (Per-Processor Interrupt) IDs (range 0x10 - 0x1f) */
+#define PPI_IRQ_OFFSET			0x10
+
+/* Secure physical generic timer IRQ (interrupt 0xd)*/
+#define SEC_TIMER_PPI_IRQ_BASE	(0xd + PPI_IRQ_OFFSET)
+#define SEC_TIMER_PPI_IRQ		(SEC_TIMER_PPI_IRQ_BASE)
+
+
+/* Definitions of IRQ SPI (Shared Peripheral Interrupt) IDs (range 0x20-0x3FC) */
+#define SPI_IRQ_OFFSET			0x20
+
+/* UAA definitons (range 0x20 - 0x23) */
+#define UAA_SPI_IRQ_DEVS		0x4
+#define UAA_SPI_IRQ_BASE		(0x0 + SPI_IRQ_OFFSET)
+#define UAA_SPI_IRQ(dev)		((dev) + UAA_SPI_IRQ_BASE)
+
+/* Watchdog interrupt (interrupt 0x24)*/
+#define GTI_WATCHDOG_SPI_IRQ_BASE		(UAA_SPI_IRQ_BASE + UAA_SPI_IRQ_DEVS)
+#define GTI_WATCHDOG_SPI_IRQ_DEVS		0x1
+#define GTI_WATCHDOG_SPI_IRQ(dev)		(GTI_WATCHDOG_SPI_IRQ_BASE)
+
+/* PEM definitions (range 0x25 - 0x30) */
+/*
+ * T81 has 3 PEMs
+ */
+#define PEM_SPI_IRQ_DEVS		0x3
+
+/*
+ * Each of PEM has 4 interrupts
+ * (INTA, INTB, INTC, INTD)
+ */
+#define PEM_SPI_IRQS_PER_DEV			0x4
+
+#define PEM_SPI_IRQ_BASE		(GTI_WATCHDOG_SPI_IRQ_BASE + GTI_WATCHDOG_SPI_IRQ_DEVS)
+#define PEM_SPI_IRQ(dev, irq)	((dev) * PEM_SPI_IRQS_PER_DEV + (irq) + PEM_SPI_IRQ_BASE)
+
+/* SMMU definitions (range 0x31 - 0x32)*/
+/*
+ * Two interrupts per SMMU instance (Global and Context).
+ * T81 has 1 SMMU instances
+ */
+#define SMMU_SPI_IRQ_DEVS		0x1
+#define SMMU_SPI_IRQS_PER_DEV	0x2
+#define SMMU_SPI_IRQ_BASE		(PEM_SPI_IRQ_BASE + (PEM_SPI_IRQ_DEVS*PEM_SPI_IRQS_PER_DEV))
+#define SMMU_SPI_IRQ(dev, irq)	((dev) * SMMU_SPI_IRQS_PER_DEV + (irq) + SMMU_SPI_IRQ_BASE)
+
+#endif /* __PLATFORM_IRQS_DEF_H__ */
