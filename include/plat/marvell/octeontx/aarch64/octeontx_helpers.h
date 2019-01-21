db57899117461b882c899e5951296be7759e2ab0
Nithya Mani <nithya.mani@cavium.com>
2019-01-21 13:11:09 -0800

    plat: octeontx: Add the missing header file in commit #826d7cc
    Change-Id: I34f3b221ab515471acd3c6f917d21cfc05a79164
    Signed-off-by: Nithya Mani <nithya.mani@cavium.com>
    Reviewed-on: https://sj1git1.cavium.com/2445
    Tested-by: sa_ip-sw-jenkins
    Reviewed-by: Kostya Porotchkin <Kostya.Porotchkin@cavium.com>

---
+++ include/plat/marvell/octeontx/aarch64/octeontx_helpers.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2018 Marvell International Ltd.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ * https://spdx.org/licenses
+ */
+
+#ifndef __OCTEONTX_HELPERS_H__
+#define __OCTEONTX_HELPERS_H__
+
+void plat_secondary_cold_boot_setup(void);
+
+#endif /* __OCTEONTX_HELPERS_H__ */
