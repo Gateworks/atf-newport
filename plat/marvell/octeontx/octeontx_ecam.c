2b2c40bf38f0e48fb2745d4f7bdd9dea97d35abb
Radha Mohan Chintakuntla <rchintakuntla@cavium.com>
2019-02-14 15:32:06 -0800

    plat: marvell: unset PHYS_NSEC bit for BPHY secure interrupt handling
    The PHYS_NSEC bit in IOBN0_DOM0_BUS6_STREAMS and
    IOBN0_DOM0_DEV0_STREAMS has to be clear for secure interrupts of BPHY
    to work.
    
    Change-Id: I08538f348f0a3ccd8b9b09f2491f33d98aababa6
    Signed-off-by: Radha Mohan Chintakuntla <rchintakuntla@cavium.com>
    Reviewed-on: https://sj1git1.cavium.com/4144
    Tested-by: sa_ip-sw-jenkins
    Reviewed-by: Nithya Mani <Nithya.Mani@cavium.com>

--- plat/marvell/octeontx/octeontx_ecam.c
+++ plat/marvell/octeontx/octeontx_ecam.c
@@ -409,15 +409,22 @@ static void init_iobn5(uint64_t config_base, uint64_t config_size)
 		domx_const.u = CSR_READ(CAVM_ECAMX_DOMX_CONST(0,domain));
 		if (domx_const.s.pres) {
 			for (bus = 0; bus < 256; bus++) {
-				/* To get secure interrupts for GPIO the PHYS_NSEC has to be 0 */
-				if (domain == 0 && bus == 0)
+				/* To get secure interrupts for GPIO and BPHY
+				 * the PHYS_NSEC has to be 0
+				 */
+				if (domain == 0 && (bus == 0 || bus == 6))
 					CSR_WRITE(CAVM_IOBNX_DOMX_BUSX_STREAMS(iobn_nr, domain, bus), 0x2);
 				else
 					CSR_WRITE(CAVM_IOBNX_DOMX_BUSX_STREAMS(iobn_nr, domain, bus), 0x3);
 			}
 			for (dev = 0; dev < 32; dev++) {
-				/* To get secure interrupts for GPIO the PHYS_NSEC has to be 0 */
-				if (domain == 0 && dev == (CAVM_PCC_DEV_CON_E_GPIO_CN9 >> 3))
+				/* To get secure interrupts for GPIO and BPHY
+				 * the PHYS_NSEC has to be 0
+				 */
+				if (domain == 0 &&
+				    (dev == (CAVM_PCC_DEV_CON_E_GPIO_CN9 >> 3)
+				    || dev == ((CAVM_PCC_DEV_CON_E_BPHY & 0xFF)
+					       >> 3)))
 					CSR_WRITE(CAVM_IOBNX_DOMX_DEVX_STREAMS(iobn_nr, domain, dev), 0x2);
 				else
 					CSR_WRITE(CAVM_IOBNX_DOMX_DEVX_STREAMS(iobn_nr, domain, dev), 0x3);
