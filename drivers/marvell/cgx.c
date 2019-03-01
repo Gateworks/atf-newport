9231babe72f1deb238347111ddbc1e5efb8011d8
Nithya Mani <nithya.mani@cavium.com>
2019-03-01 11:21:41 -0800

    drivers: marvell: cgx: disable RX EQU during CGX link bring up
    HRM recommends to perform RX adaptation for speed >= 10G
    when training is not enabled. RX EQU sequence provided
    by Serdes team has issues and being investigated. This causes
    link instabilities. Until the issue is resolved, disable RX EQU during
    CGX link bring up
    
    Change-Id: I4af8366d778ee9a8b63caa8c369364e4e30bcf73
    Signed-off-by: Nithya Mani <nithya.mani@cavium.com>
    Reviewed-on: https://sj1git1.cavium.com/5083
    Tested-by: sa_ip-sw-jenkins
    Reviewed-by: Chandrakala Chavva <Chandrakala.Chavva@cavium.com>

--- drivers/marvell/cgx.c
+++ drivers/marvell/cgx.c
@@ -1771,6 +1771,11 @@ int cgx_xaui_set_link_down(int cgx_id, int lmac_id)
 
 int cgx_rx_equalization(int cgx_id, int lmac_id)
 {
+	/* RX adapation sequence has some issues and Serdes team is
+	 * investigating it. Until then, disable RX EQU during
+	 * CGX link bring up
+	 */
+#if 0
 	int lane_idx;
 	int qlm, lane, max_lanes, timeout = 100;
 	cgx_lmac_config_t *lmac;
@@ -1858,6 +1863,7 @@ int cgx_rx_equalization(int cgx_id, int lmac_id)
 		debug_cgx("%s: %d:%d qlm %d lane %d Rx EQU done\n", __func__,
 				cgx_id, lmac_id, qlm, lane_idx);
 	}
+#endif
 	return 0;
 }
 
