83d8233eae7227dcdf158810caa31c2a5463f37a
Suneel Garapati <sgarapati@marvell.com>
2019-03-10 16:41:46 -0700

    octeontx: iobn5: permit pemx controllers to non-secure world
    Using ecamx_domx_const doesn't show up domain present for pem
    controllers which are on domain 3 4 5 6. So change logic to
    program all domains from 0 to 6 for non-secure world.
    
    Change-Id: Ie9f74421e9c92d89efc93f715f687235a47b004e
    Signed-off-by: Suneel Garapati <sgarapati@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/5462
    Tested-by: sa_ip-sw-jenkins
    Reviewed-by: Chandrakala Chavva <Chandrakala.Chavva@cavium.com>

--- plat/marvell/octeontx/octeontx_ecam.c
+++ plat/marvell/octeontx/octeontx_ecam.c
@@ -383,8 +383,7 @@ static void init_iobn5(uint64_t config_base, uint64_t config_size)
 	int i, iobn_nr;
 	int domain, bus, dev;
 	union cavm_pccpf_xxx_vsec_ctl vsec_ctl;
-	union cavm_ecamx_const ecamx_const;
-	union cavm_ecamx_domx_const domx_const;
+	int iobn5_domain_max = 0x7;
 
 	vsec_ctl.u = octeontx_read32(config_base + CAVM_PCCPF_XXX_VSEC_CTL);
 	iobn_nr = vsec_ctl.s.inst_num;
@@ -402,33 +401,35 @@ static void init_iobn5(uint64_t config_base, uint64_t config_size)
 		CSR_WRITE(CAVM_IOBNX_RSLX_STREAMS(iobn_nr,i), 0x3);
 	}
 
-	ecamx_const.u = CSR_READ(CAVM_ECAMX_CONST(0));
-
-	for (domain = 0; domain < ecamx_const.s.domains; domain++) {
-		/* Domains may not be contiguous */
-		domx_const.u = CSR_READ(CAVM_ECAMX_DOMX_CONST(0,domain));
-		if (domx_const.s.pres) {
-			for (bus = 0; bus < 256; bus++) {
-				/* To get secure interrupts for GPIO and BPHY
-				 * the PHYS_NSEC has to be 0
-				 */
-				if (domain == 0 && (bus == 0 || bus == 6))
-					CSR_WRITE(CAVM_IOBNX_DOMX_BUSX_STREAMS(iobn_nr, domain, bus), 0x2);
-				else
-					CSR_WRITE(CAVM_IOBNX_DOMX_BUSX_STREAMS(iobn_nr, domain, bus), 0x3);
-			}
-			for (dev = 0; dev < 32; dev++) {
-				/* To get secure interrupts for GPIO and BPHY
-				 * the PHYS_NSEC has to be 0
-				 */
-				if (domain == 0 &&
-				    (dev == (CAVM_PCC_DEV_CON_E_GPIO_CN9 >> 3)
-				    || dev == ((CAVM_PCC_DEV_CON_E_BPHY & 0xFF)
-					       >> 3)))
-					CSR_WRITE(CAVM_IOBNX_DOMX_DEVX_STREAMS(iobn_nr, domain, dev), 0x2);
-				else
-					CSR_WRITE(CAVM_IOBNX_DOMX_DEVX_STREAMS(iobn_nr, domain, dev), 0x3);
-			}
+	/*
+	 * ECAMX_DOMX_CONST shows up only that are present under it.
+	 * To enable PEMX controllers on domain 3 4 5 6, use the loop max
+	 * as 0x7 to include them.
+	 */
+	for (domain = 0; domain < iobn5_domain_max; domain++) {
+		for (bus = 0; bus < 256; bus++) {
+			/* To get secure interrupts for GPIO and BPHY
+			 * the PHYS_NSEC has to be 0
+			 */
+			if (domain == 0 && (bus == 0 || bus == 6))
+				CSR_WRITE(CAVM_IOBNX_DOMX_BUSX_STREAMS(
+					  iobn_nr, domain, bus), 0x2);
+			else
+				CSR_WRITE(CAVM_IOBNX_DOMX_BUSX_STREAMS(
+					  iobn_nr, domain, bus), 0x3);
+		}
+		for (dev = 0; dev < 32; dev++) {
+			/* To get secure interrupts for GPIO and BPHY
+			 * the PHYS_NSEC has to be 0
+			 */
+			if (domain == 0 &&
+			    (dev == (CAVM_PCC_DEV_CON_E_GPIO_CN9 >> 3)
+			    || dev == ((CAVM_PCC_DEV_CON_E_BPHY & 0xFF) >> 3)))
+				CSR_WRITE(CAVM_IOBNX_DOMX_DEVX_STREAMS(
+					  iobn_nr, domain, dev), 0x2);
+			else
+				CSR_WRITE(CAVM_IOBNX_DOMX_DEVX_STREAMS(
+					  iobn_nr, domain, dev), 0x3);
 		}
 	}
 }
