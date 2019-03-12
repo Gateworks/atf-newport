350f69e19886b6b60fddf70167de6f8ccc6fa3a3
Felix Manlunas <fmanlunas@marvell.com>
2019-03-12 13:20:38 -0700

    drivers: marvell: phy: Add 88X6141 API library and header files
    libphy_88x6141.a contains pre-compiled objects sourced from MYD API release
    1.2 and PHY firmware images from X7120_A0_FW_1093_Delivery.zip.
    
    This new code does not yet get built in to ATF.  That will happen in an
    upcoming commit for the actual 6141 PHY driver.
    
    Change-Id: I05b28e040c471c434b622267271ee697e9722a60
    Signed-off-by: Felix Manlunas <fmanlunas@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/5576
    Tested-by: Nithya Mani <Nithya.Mani@cavium.com>
    Reviewed-by: Nithya Mani <Nithya.Mani@cavium.com>

---
+++ lib/libphy/marvell_88x6141/include/mydFwImages.h
@@ -0,0 +1,16 @@
+/*******************************************************************************
+Copyright (C) 2014 - 2019, Marvell International Ltd. and its affiliates
+If you received this File from Marvell and you have entered into a commercial
+license agreement (a "Commercial License") with Marvell, the File is licensed
+to you under the terms of the applicable Commercial License.
+*******************************************************************************/
+
+#ifndef MYD_FWIMAGES_H
+#define MYD_FWIMAGES_H
+
+extern unsigned short x6141_sbus_master_image_start[];
+extern unsigned short x6141_sbus_master_image_end[];
+extern unsigned short x6141_serdes_image_start[];
+extern unsigned short x6141_serdes_image_end[];
+
+#endif
