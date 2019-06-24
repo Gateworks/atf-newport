ed18b86b3850de6aced8fe9c6a6abf61906c1c55
Tomasz Michalec <tomasz@marvell.com>
2019-06-24 13:14:31 +0200

    plat: marvell: octeontx: otx2: loki: Enable SCMI probe
    Change-Id: If23bb8e0f890e0b6dfc9a667cad0f03e6aca1f64
    Signed-off-by: Tomasz Michalec <tomasz@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/11515
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>

--- plat/marvell/octeontx/otx2/plat_pwrc.c
+++ plat/marvell/octeontx/otx2/plat_pwrc.c
@@ -332,15 +332,8 @@ int octeontx_pwrc_setup(void)
 
 	scmi_channel.info = &plat_octeontx_scmi_plat_info;
 	scmi_channel.lock = &octeontx_scmi_lock;
+	scmi_handle = scmi_init(&scmi_channel);
 
-	/*
-	 * FIXME: Until SCP will work in ASIM for loki platform, skip scmi init
-	 */
-	if (IS_OCTEONTX_PN(read_midr(), LOKIPARTNUM)) {
-		scmi_handle = NULL;
-	} else {
-		scmi_handle = scmi_init(&scmi_channel);
-	}
 	if (scmi_handle == NULL) {
 		NOTICE("SCMI Initialization failed, fallback to legacy PM\n");
 		return -1;
